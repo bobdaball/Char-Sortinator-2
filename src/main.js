c4cb70ff7bc3d403fcc32c75da62d3b57c34751a
Alexander Mire <amire51117@gmail.com>
2018-01-25 14:44:13 -0500

    add react

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import InputForm from './Form';
+
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(
+    React.createElement(InputForm),
+    document.getElementById('mount')
+  );
+});
